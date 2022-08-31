8f71329e25f0079ca773430e8f60b5d148b200a3
kapilmahajan123 <montyempire123gmail.com>
2022-08-31 21:28:19 +0530

    [Kapil]-program EmpWage For counter

---
+++ EmpWageCounter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+counter=0
+Fruits[((counter++))]="apple"
+Fruits[((counter++))]="orange"
+Fruits[((counter++))]="mango"
+Fruits[((counter++))]="jackfruits"
+
+echo ${Fruits[@]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
